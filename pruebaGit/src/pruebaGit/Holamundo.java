7585adb1458b54f26a210c9e39ab40e21d71c920
mikel Oyarzabal <mikel.oyarzabal@opendeusto.es>
2023-09-27 10:49:59 +0200

    quitar un espacio

--- pruebaGit/src/pruebaGit/Holamundo.java
+++ pruebaGit/src/pruebaGit/Holamundo.java
@@ -1,17 +1,11 @@
 package pruebaGit;
 
 public class Holamundo {
-<<<<<<< HEAD
 
     public static void main(String[] args) {
 		    System.out.println("Hola mundo!!");
 	  
     }
-=======
-	public static void main(String[] args) {
-		System.out.println("Hola mundo!!!!");
-	}
->>>>>>> branch 'main' of https://github.com/MikelOyarzabal/prueba.git
 
 	public int sumar(int a, int b) {
 		return a + b;
