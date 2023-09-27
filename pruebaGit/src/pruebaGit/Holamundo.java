854eae51a06ed0a06675da501a6e8ffc7f2a0acc
MikelOyarzabal <145966883+MikelOyarzabal@users.noreply.github.com>
2023-09-27 10:46:45 +0200

    añadir una exclamacion

--- pruebaGit/src/pruebaGit/Holamundo.java
+++ pruebaGit/src/pruebaGit/Holamundo.java
@@ -2,7 +2,7 @@ package pruebaGit;
 
 public class Holamundo {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!!");
+		System.out.println("Hola mundo!!!!");
 	}
 
 	public int sumar(int a, int b) {
